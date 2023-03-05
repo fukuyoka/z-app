3c0cb7bf55cd99bfc5c28f856f65f05e7082ece9
fukuyoka <fukuyoka88@gmail.com>
2023-03-05 17:37:30 +0900

    5回目

--- index.tsx
+++ index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>HELLO WORLD</h1>
-      <h1>編集しました</h1>
+      <h1>編集しまし</h1>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
